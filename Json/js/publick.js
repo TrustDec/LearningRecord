f38738b77ce01f6a431c1491576904a826d28c65
smallrui1 <603049583@qq.com>
2016-07-30 18:01:04 +0800

    JSON入门参考笔记,后期做详细笔记

---
+++ Json/js/publick.js
@@ -0,0 +1,7 @@
+/*为方便测试封装的方法,忽略即可*/
+	function echo(str){
+		return document.writeln("<strong style='color:red;font-size:18px;'>"+str+"<strong>");
+	}
+	function cl(str){
+		return console.log(str);
+	}
